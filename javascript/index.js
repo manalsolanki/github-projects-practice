6758e795d799073fc4702c875f40cdd665b6e236
Manal Solanki <60120657+manalsolanki@users.noreply.github.com>
2020-03-13 11:02:13 -0400

    Updated the basic functionality of JS (#7)
    Co-authored-by: Manal Upendra Solanki <n01397767@nj-00118-019.humber.org>

---
+++ javascript/index.js
@@ -0,0 +1,12 @@
+
+function colorChange(){
+    //console.log ("hello")
+    let helloHeading = document.querySelector('#main-page-heading')
+    //console.log(helloHeading)
+    helloHeading.style.color="red";
+}
+function normalColor(){
+    let helloHeading = document.querySelector('#main-page-heading')
+    helloHeading.style.color="rgb(13, 45, 99)";
+
+}
